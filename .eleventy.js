c43cbcdd4b5b659bb5b7f371466111ccef752797
Reinhardt van Rooyen <reinhardt@thelabs.co.za>
2024-01-24 22:26:04 +0200

    imrpoved caching of passages

--- .eleventy.js
+++ .eleventy.js
@@ -2,16 +2,27 @@ const fs = require("fs");
 const htmlmin = require("html-minifier");
 const dumpFilter = require("@jamshop/eleventy-filter-dump");
 const {parse} = require("csv-parse/sync");
-
+const { EleventyRenderPlugin } = require("@11ty/eleventy");
+const pluginWebc = require("@11ty/eleventy-plugin-webc");
 module.exports = function(eleventyConfig) {
 
   if (process.env.ELEVENTY_PRODUCTION) {
     eleventyConfig.addTransform("htmlmin", htmlminTransform);
   }
 
+  eleventyConfig.addPassthroughCopy({
+    './node_modules/alpinejs/dist/cdn.js': './js/alpine.js',
+  })
+
   eleventyConfig.addFilter("dump", dumpFilter);
+  
 
-
+  eleventyConfig.addPlugin(pluginWebc, {
+		components: [
+			"./_components/**/*.webc",
+			"npm:@11ty/is-land/*.webc"
+		]
+	});
   // add support for tsv
   eleventyConfig.addDataExtension("tsv", (contents) => {
   const records = parse(contents, {
@@ -21,6 +32,8 @@ module.exports = function(eleventyConfig) {
   });
   return records;
 });
+
+eleventyConfig.addPlugin(EleventyRenderPlugin);
 
   // Passthrough
   eleventyConfig.addPassthroughCopy({ "src/static": "." });
